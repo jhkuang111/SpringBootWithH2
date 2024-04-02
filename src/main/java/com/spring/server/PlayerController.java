1428bb36e3896ba887abeb43bc7dda2a1021d6d7
jhkuang111 <jhkuang11@yahoo.com>
2024-04-01 19:14:34 -0700

    Add GET API to retrieve by ID

--- src/main/java/com/spring/server/PlayerController.java
+++ src/main/java/com/spring/server/PlayerController.java
@@ -1,15 +1,20 @@
 package com.spring.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @RestController
 public class PlayerController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     PlayerService playerService;
@@ -23,5 +28,14 @@ public class PlayerController {
     public Player addPlayer(@RequestBody Player newPlayer) {
         newPlayer.setId(0);
         return playerService.addPlayer(newPlayer);
+    }
+
+    @GetMapping("/players/{id}")
+    public Player getPlayerById(@PathVariable int id) {
+        Player player = playerService.getPlayerById(id);
+        if (player == null) {
+            logger.warn("Player with id {} is not found", id);
+        }
+        return player;
     }
 }
--- src/main/java/com/spring/server/PlayerService.java
+++ src/main/java/com/spring/server/PlayerService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlayerService {
@@ -20,4 +21,14 @@ public class PlayerService {
     public Player addPlayer(Player player) {
         return playerRepo.save(player);
     }
+
+    // Get player by ID
+    public Player getPlayerById(int id) {
+        Optional<Player> player = playerRepo.findById(id);
+        if (player.isEmpty()) {
+            throw new RuntimeException("Player with id {"+ id +"} not found");
+        }
+        return player.get();
+    }
+
 }
